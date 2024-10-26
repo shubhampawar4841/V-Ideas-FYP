df3e89a2818589fb1f36abcb2512def11cbc6715
Shubham Pawar <114252697+shubhampawar4841@users.noreply.github.com>
2024-10-26 20:07:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,11 +27,11 @@ app.use('/api/guide', guideRoutes); // Routes related to guides
 app.use('/api/admin', adminRoutes); // Routes related to admin
 
 // // Error handling for non-existent routes
-// app.use((req, res, next) => {
-//   const error = new Error('Not Found');
-//   res.status(404);
-//   next(error);
-// });
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  res.status(404);
+  next(error);
+});
 
 // Custom error handler
 app.use((error, req, res, next) => {
